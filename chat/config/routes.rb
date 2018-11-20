148bbee28d0d68b157d38cf23ce13330688a0487
root <aikanshgarg0303@gmail.com>
2018-11-20 18:59:42 +0530

    part3

--- chat/config/routes.rb
+++ chat/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # rest of the nested routes were made by us
   resources :chatrooms do
   	resource :chatroom_users
+  	resources :messages
   end
 
   root 'chatrooms#index'
