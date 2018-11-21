199cdcfaec8446224caaae24ae0f98c15dd5e490
root <aikanshgarg0303@gmail.com>
2018-11-21 12:53:40 +0530

    voila! backend done!

--- chat/app/assets/javascripts/chatrooms.coffee
+++ chat/app/assets/javascripts/chatrooms.coffee
@@ -7,6 +7,13 @@ $(document).on "turbolinks:load", ->
   if e && e.keyCode == 13
    e.preventDefault()
    $(this).submit()
+
  $("#new_message").on "submit", (e) ->
   e.preventDefault()
-  console.log "FINALLY"
+
+  chatroom_id = $("[data-behaviour='messages']").data("chatroom-id")
+  body = $("#message_body")
+
+  App.chatrooms.send_message(chatroom_id, body.val())
+
+  body.val("")
--- chat/app/channels/chatrooms_channel.rb
+++ chat/app/channels/chatrooms_channel.rb
@@ -11,5 +11,18 @@ class ChatroomsChannel < ApplicationCable::Channel
   	stop_all_streams
     # Any cleanup needed when channel is unsubscribed
   end
+
+# the name of this method and data argument passed comes from this channel's chatrooms.coffee, not the original
+# this performs the exact same action that our controller's action of create did
+
+  def send_message(data)
+    @chatroom = Chatroom.find(data["chatroom_id"])
+    message = @chatroom.messages.create(body: data["body"], user: current_user)
+    MessageRelayJob.perform_later(message)
+    #Rails.logger.info data
+  end
+
+
+
 end
  
